54b515996bfc3785759029c944c8ba1622333233
Javier Lopatin <javierlopatin@gmail.com>
2018-08-23 14:52:31 +0200

    Update installPrograms.sh

--- installPrograms.sh
+++ installPrograms.sh
@@ -10,23 +10,13 @@ sudo apt-get upgrade
 bash Miniconda3-latest-Linux-x86_64.sh
 
 # install python libraries
-conda install -c anaconda spyder -y
-conda install -c anaconda jupyter -y
-conda install -c anaconda numpy -y
-conda install -c anaconda scipy -y
-conda install -c anaconda pandas -y
-conda install -c anaconda scikit-learn -y
-conda install -c anaconda scikit-image -y
-conda install -c conda-forge gdal -y
-conda install -c conda-forge rasterio -y
-conda install -c conda-forge rasterstats -y
+conda install -c anaconda spyder jupyter numpy scipy pandas scikit-learn scikit-image -y
+conda install -c conda-forge gdal rasterio rasterstats pyshp rsgislib tuiview tqdm -y
 conda install -c rbacher pysptools -y
-conda install -c conda-forge pyshp -y
 conda install -c ioos geopandas -y
-conda install -c conda-forge rsgislib -y
-conda install -c conda-forge tuiview -y
-conda install -c conda-forge tqdm -y
 conda update --all -y
+
+pip install sentinelhub --upgrade
 
 # install R and RStudio
 conda install -c r r-base -y
